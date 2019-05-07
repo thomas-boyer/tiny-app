9f619d27c7d78f3d3f0ad4e31aad526ac579e5a9
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-07 22:47:11 +0000

    Imported bodyParser, handled GET request for urls/new, and added randomString() function

--- express_server.js
+++ express_server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
@@ -10,16 +14,34 @@ const urlDatabase =
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) =>
+function generateRandomString()
+{
+  let result = "";
+
+  for (let i = 0; i < 6; i++)
   {
-    res.send('Hello!');
+    //Declare a number that will correspond to either the digits 0-9
+    //or an ASCII character code from A-Z or a-z
+    let rand;
+
+    //Generate a random number between 55 and 123
+    rand = Math.floor(Math.random() * 68) + 55;
+    //If rand is between 55 and 64 inclusive, set it to a digit between 0 and 9
+    if (rand <= 64) result += String(rand - 55);
+    //Filter out non-alphabetical ASCII codes between A-Z and a-z
+    else if (rand >= 91 && rand <= 96) i--;
+    //Otherwise, add corresponding ASCII value to returned string
+    else result += String.fromCharCode(rand);
+  }
+
+  return result;
+}
+
+app.get('/urls/new', (req, res) =>
+  {
+    res.render('urls_new');
   });
 
-app.get('/urls', (req, res) =>
-  {
-    const templateVars = { urls: urlDatabase };
-    res.render('urls_index', templateVars);
-  });
 
 app.get('/urls/:shortURL', (req, res) =>
   {
@@ -32,6 +54,23 @@ app.get('/urls.json', (req, res) =>
     res.json(urlDatabase);
   });
 
+app.get('/urls', (req, res) =>
+  {
+    const templateVars = { urls: urlDatabase };
+    res.render('urls_index', templateVars);
+  });
+
+app.post('/urls', (req, res) =>
+  {
+    console.log(req.body);
+    res.send("Ok");
+  });
+
+app.get('/', (req, res) =>
+  {
+    res.send('Hello!');
+  });
+
 app.get('/hello', (req, res) =>
   {
     res.send('<html><body>Hello <b>World</b></body></html>\n')
@@ -41,3 +80,5 @@ app.listen(PORT, () =>
   {
     console.log(`Example app listening on port ${PORT}!`);
   });
+
+console.log(generateRandomString());
