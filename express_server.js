149a09342c418813061d9c41379ec395ec0037c5
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-08 17:05:30 +0000

    Replaced all dead links with functioning links

--- express_server.js
+++ express_server.js
@@ -16,24 +16,13 @@ const urlDatabase =
 
 function generateRandomString()
 {
-  let result = "";
+  let result = '';
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
   for (let i = 0; i < 6; i++)
   {
-    //Declare a number that will correspond to either the digits 0-9
-    //or an ASCII character code from A-Z or a-z
-    let rand;
-
-    //Generate a random number between 55 and 123
-    rand = Math.floor(Math.random() * 68) + 55;
-    //If rand is between 55 and 64 inclusive, set it to a digit between 0 and 9
-    if (rand <= 64) result += String(rand - 55);
-    //Filter out non-alphabetical ASCII codes between A-Z and a-z
-    else if (rand >= 91 && rand <= 96) i--;
-    //Otherwise, add corresponding ASCII value to returned string
-    else result += String.fromCharCode(rand);
+     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
-
   return result;
 }
 
@@ -81,7 +70,7 @@ app.post('/urls', (req, res) =>
   {
     const randString = generateRandomString();
     urlDatabase[randString] = req.body.longURL;
-    res.redirect(`/url/${randString}`);
+    res.redirect(`/urls/${randString}`);
     res.end();
   });
 
